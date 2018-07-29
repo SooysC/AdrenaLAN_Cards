363feb9818201494f4330d3113b55281de4ddd28
Shayan Hashemzehi <shayan77525@gmail.com>
2018-07-29 05:19:03 -0400

    first syncvar testing

---
+++ AdrenaLanProject/Assets/Queue.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Queue : NetworkBehaviour {
+
+
+
+
+    [SyncVar]
+    public string history = "";
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    void OnCardPlayed()
+    {
+
+    }
+	// Update is called once per frame
+	void Update () {
+        RpcSyncVarWithClients(history);
+        Debug.Log(history);
+	}
+    [ClientRpc]
+    void RpcSyncVarWithClients(string varToSync)
+    {
+        history = varToSync;
+    }
+}
