0418788326f57a207cf417b9c7c14229d1a28975
mikeshi993 <mikehxshi@gmail.com>
2018-07-28 20:31:50 -0400

    state scripts ready

---
+++ AdrenaLanProject/Assets/Scripts/DeerAppeer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeerAppeer : MonoBehaviour {
+
+	public GameObject[] Homes;
+    public int Target_Destination;
+	public GameObject Deer;
+	public GameObject board;
+    public bool run = false;
+	private bool set = false;
+
+	void Start () {        
+    	StartCoroutine(generateDeer());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (set) {
+            GoIntoHouse();
+        }
+    }
+
+	public void GoIntoHouse () {
+		GameObject d = GameObject.Find("Deer(Clone)");
+		Vector3 path = Homes[Target_Destination].transform.position - d.transform.position;
+        d.GetComponent<Rigidbody>().velocity = Vector3.Lerp(d.GetComponent<Rigidbody>().velocity, path, 0.1f);
+        Quaternion targetRotation = Quaternion.LookRotation(path);
+        d.transform.rotation = Quaternion.Lerp(d.transform.rotation, targetRotation, 0.2f);
+
+		if (path.magnitude <= 0.001){
+			//set = false;
+			//Destroy(GameObject.Find("Deer"));
+		}
+	}
+
+    public IEnumerator generateDeer() {
+        yield return new WaitUntil(() => run);
+		Instantiate(Deer, board.transform);
+		yield return new WaitForSeconds(3f);
+		set = true;
+    }
+}
---
+++ AdrenaLanProject/Assets/Scripts/GameTimeline.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameTimeline : MonoBehaviour {
+    public bool gameStarted = false;
+    public GameObject gameBoard;
+	public enum Game_State {game_setup, game_start, player_a_phase, player_b_phase, game_over};
+	public Game_State current_state = Game_State.game_setup;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void nextState () {
+		switch (current_state) {
+			case Game_State.game_setup:
+				current_state = Game_State.game_start;
+				break;
+			case Game_State.game_start:
+				current_state = Game_State.player_a_phase;
+				break;
+			case Game_State.player_a_phase:
+				current_state = Game_State.player_b_phase;
+				break;
+			case Game_State.player_b_phase:
+				// For now we'll make the game one round only
+				current_state = Game_State.game_over;
+				break;
+			case Game_State.game_over:
+				current_state = Game_State.game_over;
+				break;
+			default:
+				Debug.Log("Should not go here.");
+				break;
+		}
+	}
+}
